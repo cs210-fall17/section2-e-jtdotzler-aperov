b7bffc91a9385da89f2768901a32cb9178342df5
James John Dotzler <jtdotzler@mastiff.cs.arizona.edu>
2017-09-11 17:00:48 -0700

    Decapitalized "out"

--- Hello.java
+++ Hello.java
@@ -9,7 +9,7 @@ public class Hello {
     }
 
     public static void callSign() {
-        System.Out.println("Stabilize you rear deflectors... Watch for enemy fighters.");
+        System.out.println("Stabilize you rear deflectors... Watch for enemy fighters.");
 	System.out.println("Lock S-foils in attack position.");
         // Gold Leader should add a print statement that says "Stabilize your rear deflectors... Watch for enemy fighters."
 
